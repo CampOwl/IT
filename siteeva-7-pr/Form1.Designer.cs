b7d36a323cd66bbdc1fc647d3315fd73972d228e
Анастасия Ситеева <nastya-zmeika@mail.ru>
2021-03-22 15:48:38 +0300

    IT

---
+++ siteeva-7-pr/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace siteeva_7_pr
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Обязательная переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Требуемый метод для поддержки конструктора — не изменяйте 
+    /// содержимое этого метода с помощью редактора кода.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.dataGridView1 = new System.Windows.Forms.DataGridView();
+      this.button1 = new System.Windows.Forms.Button();
+      this.button2 = new System.Windows.Forms.Button();
+      this.table2 = new System.Windows.Forms.DataGridView();
+      this.gen_table2 = new System.Windows.Forms.Button();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.table2)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // dataGridView1
+      // 
+      this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+      this.dataGridView1.Name = "dataGridView1";
+      this.dataGridView1.RowHeadersWidth = 10;
+      this.dataGridView1.Size = new System.Drawing.Size(654, 194);
+      this.dataGridView1.TabIndex = 0;
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(734, 111);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(75, 23);
+      this.button1.TabIndex = 1;
+      this.button1.Text = "запуск";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // button2
+      // 
+      this.button2.Location = new System.Drawing.Point(734, 82);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(75, 23);
+      this.button2.TabIndex = 2;
+      this.button2.Text = "генерация";
+      this.button2.UseVisualStyleBackColor = true;
+      this.button2.Click += new System.EventHandler(this.button2_Click);
+      // 
+      // table2
+      // 
+      this.table2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.table2.Location = new System.Drawing.Point(12, 212);
+      this.table2.Name = "table2";
+      this.table2.RowHeadersWidth = 10;
+      this.table2.Size = new System.Drawing.Size(654, 194);
+      this.table2.TabIndex = 3;
+      // 
+      // gen_table2
+      // 
+      this.gen_table2.Location = new System.Drawing.Point(720, 326);
+      this.gen_table2.Name = "gen_table2";
+      this.gen_table2.Size = new System.Drawing.Size(75, 23);
+      this.gen_table2.TabIndex = 4;
+      this.gen_table2.Text = "генерация";
+      this.gen_table2.UseVisualStyleBackColor = true;
+      this.gen_table2.Click += new System.EventHandler(this.gen_table2_Click);
+      // 
+      // textBox1
+      // 
+      this.textBox1.Location = new System.Drawing.Point(720, 285);
+      this.textBox1.Multiline = true;
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(234, 20);
+      this.textBox1.TabIndex = 5;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(1028, 450);
+      this.Controls.Add(this.textBox1);
+      this.Controls.Add(this.gen_table2);
+      this.Controls.Add(this.table2);
+      this.Controls.Add(this.button2);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.dataGridView1);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.table2)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView table2;
+        private System.Windows.Forms.Button gen_table2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ siteeva-7-pr/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace siteeva_7_pr
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+    int[,] mas = new int[4, 4];
+    int LIM = 4;
+    //В строке матрицы с максимальным элементом  обнулить все элементы, кроме максимального.
+    // матрица из четных строк и нечетных столбцов исходной матрицы и найти определитель
+    private void button1_Click(object sender, EventArgs e)
+    {
+      int[] max_index = new int[LIM];
+      max_index[0] = 0;
+      int max_elem = mas[0, 0];
+      for (int i = 1; i < LIM; i++)
+        for (int j = 0; j < LIM; j++)
+          if (max_elem < mas[i,j])
+          {
+            max_elem = mas[i, j];
+            max_index[0] = i;
+          }
+
+      int k = 1;
+      for (int i = max_index[0] + 1; i< LIM; i++)
+        for (int j = 0; j < LIM; j++)
+          if (max_elem == mas[i, j]) max_index[k++] = i;
+
+      for (int key = 0; key < k; key++)
+        for (int j = 0; j < LIM; j++)
+          if (mas[max_index[key],j] != max_elem)
+          {
+            dataGridView1.Rows[max_index[key]].Cells[j].Value = '0';
+          }
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+      dataGridView1.RowCount = LIM;
+      dataGridView1.ColumnCount = LIM;
+
+      Random rand = new Random();
+      for (int i = 0; i < LIM; i++)
+        for (int j = 0; j < LIM; j++)
+        {
+          mas[i, j] = rand.Next(-50, 50);
+          dataGridView1.Rows[i].Cells[j].Value = Convert.ToString(mas[i, j]);
+        }
+    }
+    public int[,] less_mat(int[,] mat, int size, int ik, int jk)
+    {
+      int[,] new_mat = new int[size - 1, size - 1];
+      int k = 0;
+      int l = 0;
+      for (int i = 0; i < size; i++)
+      {
+        for (int j = 0; j < size; j++)
+        {
+          if ((i != ik) && (j != jk))
+          {
+            new_mat[k, l] = mat[i, j];
+            l++;
+          }
+        }
+        if (i != ik) k++;
+      }
+        
+      return new_mat;
+    }
+    public int deter(int[,] mat, int size)
+    {
+      if (size == 2)
+      {
+        return mat[0, 0] * mat[1, 1] - mat[1, 0] * mat[0, 1];
+      }
+      else
+      {
+        int det = 0;
+        for (int i = 0; i < size; i++)
+          if (i % 2 == 1)
+          {
+            det -= mat[0, i] * deter(less_mat(mat, size, 0, i), size - 1);
+          }
+          else
+          {
+            det += mat[0, i] * deter(less_mat(mat, size, 0, i), size - 1);
+          }
+        return det;
+      }
+      
+    }
+
+    private void gen_table2_Click(object sender, EventArgs e)
+    {
+      int[,] new_mas = new int[LIM / 2, LIM / 2];
+      table2.RowCount = LIM/2;
+      table2.ColumnCount = LIM/2;
+
+      int k = 0;
+      int l = 0;
+      for (int i = 0; i < LIM/2; i ++)
+      {
+        for (int j = 0; j < LIM/2; j ++)
+        {
+          new_mas[i, j] = mas[i*2+1, j*2];
+          table2.Rows[i].Cells[j].Value = Convert.ToString(new_mas[i, j]);
+          l++;
+        }
+        k++;
+      }
+      textBox1.Text = "определитель матрицы равен = " + Convert.ToString(deter(new_mas,LIM/2));
+    }
+  }
+}
